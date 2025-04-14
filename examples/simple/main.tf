80233a43076e11324499b637ee64056251cb8d06
Rob Woolley <rob.woolley@windriver.com>
2025-04-14 07:58:54 -0400

    Extend the simple example to be functional
    This adds the following:
    
    * Use the us-east-1 AWS region
    * Add a NAT gateway
    * Add an instance to the private subnet
    * Automatically query for the AMI to use
    * Use cloud-init to set the username and password so that one may log
      into the EC2 Serial Console
    * Use t3.micro to ensure access to the EC2 Serial Console
    * Add a security group to the instance that allows outbound connections
    * Add the http endpoint to enforce using IMDSv2 to prevent SSRF attacks
      or accidental credential leaks via IMDSv1
    
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -6,10 +6,10 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = "ex-${basename(path.cwd)}"
-  region = "eu-west-1"
+  region = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  # azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Example    = local.name
@@ -28,8 +28,76 @@ module "vpc" {
   name = local.name
   cidr = local.vpc_cidr
 
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = local.tags
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.7.20250331.0-kernel-6.1-x86_64"]
+  }
+}
+
+data "cloudinit_config" "server_config" {
+  gzip          = true
+  base64_encode = true
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/cloud-init.tftpl", {
+      username = var.instance_username
+      password = var.instance_password
+    })
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow all outbound for NAT access"
+  vpc_id      = module.vpc.vpc_id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+module "private_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.5.0"
+
+  name = "private-instance"
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  subnet_id = module.vpc.private_subnets[0]
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = false
+
+  metadata_options = {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  user_data = "${data.cloudinit_config.server_config.rendered}"
+
+  tags = {
+    Terraform = "true"
+  }
 }
---
+++ examples/simple/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_username" {
+  description = "Instance username"
+  type        = string
+  sensitive   = true
+}
+
+variable "instance_password" {
+  description = "Instance password"
+  type        = string
+  sensitive   = true
+}
